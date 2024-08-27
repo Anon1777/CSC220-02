0e733a3cae0666335c870c76af25e73a259a8788
Anon1777 <farrelj5@tcnj.edu>
2024-08-27 15:06:42 -0400

    Projects 1 and 2

---
+++ Project 2 08272024/Lab.java
@@ -0,0 +1,6 @@
+// Lab.java
+public class Lab {
+    public static void main(String[] args){
+        System.out.println("It works"); // prints "It works" and then a new line
+    } // closes main
+} // closes Lab
