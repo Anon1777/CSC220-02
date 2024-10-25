6553ea17594f1f191bc555b6e917e3f9e76dcb3e
Anon1777 <farrelj5@tcnj.edu>
2024-10-25 09:39:22 -0400

    delete ungraded in class work

--- Project 2 08272024/Lab.java
+++
@@ -1,6 +0,0 @@
-// Lab.java
-public class Lab {
-    public static void main(String[] args){
-        System.out.println("It works"); // prints "It works" and then a new line
-    } // closes main
-} // closes Lab
